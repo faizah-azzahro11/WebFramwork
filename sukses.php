17e2df113dcae83d69d8489873037cc380679388
faizah-azzahro11 <99701229+faizah-azzahro11@users.noreply.github.com>
2022-02-20 21:56:56 +0700

    pengenalan html

---
+++ sukses.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>SUKSES REGISTRASI</title>
+</head>
+<body style="background-color: grey;">
+
+	    <h1 style="color: dark;
+              justify-content: center;
+              background-color: grey;
+              display: flex;">
+              <img src="logoUB.png">
+    SELAMAT KAMU SUDAH MELAKUKAN PENDAFTARAN</h1>
+
+    <h4 style="justify-content: center;"></h4>
+
+    
+</body>
+</html>
